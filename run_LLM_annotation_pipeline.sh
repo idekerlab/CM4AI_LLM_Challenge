b8a7ad6fc5ff2c1dd8a297d024096c2d90a4de11
Chris Churas <churas.camera@gmail.com>
2024-11-05 12:20:06 -0800

    fixed some typos

--- run_LLM_annotation_pipeline.sh
+++ run_LLM_annotation_pipeline.sh
@@ -1,15 +1,16 @@
 #!/bin/bash 
-source activate llm_challenge
 
 input_file='data/GO_term_analysis/toy_example.csv'
 set_index='GO'
 gene_column='Genes'
 gene_sep=' '
+input_sep=','
 start=0
 end=11   
 # Define model options
-model_options=('mixtral_instruct' 'llama2_70b')
+model_options=('mistral_7b' 'llama2_13b')
 for model in "${model_options[@]}"; do 
     output_file="data/${model}_processed_toy_example"
     config_file="jsonFiles/toyexample_${model}.yaml"
-    python query_llm_for_analysis.py --config $config_file --input_file $input_file --initialize --set_index $set_index --gene_column $gene_column --gene_sep $gene_sep --run_contaminated --start $start --end $end --model $model --output_file $output_file
+    python query_llm_for_analysis.py --config $config_file --input $input_file --input_sep "$input_sep" --initialize --set_index $set_index --gene_column $gene_column --gene_sep "$gene_sep" --run_contaminated --start $start --end $end --model $model --output_file $output_file
+done
