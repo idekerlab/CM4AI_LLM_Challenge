62bc72dcad66eecdc36f0f106800ef6fb0d6f717
Clara Hu <mhu@health.ucsd.edu>
2024-11-05 12:21:36 -0800

    fix typo

--- run_LLM_annotation_pipeline.sh
+++ run_LLM_annotation_pipeline.sh
@@ -12,5 +12,5 @@ model_options=('mistral_7b' 'llama2_13b')
 for model in "${model_options[@]}"; do 
     output_file="data/${model}_processed_toy_example"
     config_file="jsonFiles/toyexample_${model}.yaml"
-    python query_llm_for_analysis.py --config $config_file --input $input_file --input_sep "$input_sep" --initialize --set_index $set_index --gene_column $gene_column --gene_sep "$gene_sep" --run_contaminated --start $start --end $end --model $model --output_file $output_file
+    python query_llm_for_analysis.py --config $config_file --input $input_file --input_sep "$input_sep" --initialize --set_index $set_index --gene_column $gene_column --gene_sep "$gene_sep" --run_contaminated --start $start --end $end --output_file $output_file
 done
