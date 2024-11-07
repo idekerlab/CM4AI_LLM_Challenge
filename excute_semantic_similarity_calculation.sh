36ac86987d5ee1a368b598bf808eb36543dabfa4
Chris Churas <churas.camera@gmail.com>
2024-11-07 10:34:29 -0800

    bug fixes

--- excute_semantic_similarity_calculation.sh
+++ excute_semantic_similarity_calculation.sh
@@ -1,17 +1,15 @@
 #!/bin/bash -l
 
-source activate llm_challenge
-
 # Loop over files and calculate semantic similarity
 for file in data/*processed_toy_example.tsv; do
 # Run the Python script for each file
-    model=$(basename "$file" .tsv | awk -F'_' '{print $(NF-1)"_"$NF}')
-    model_name_col='$(model)_default Name'
+    fname=`basename $file`
+    model=`echo $fname | sed "s/_processed.*//"`
+    model_name_col="${model}_default Name"
     GO_name_col='Term_Description'
     echo "Processing $file"
+    echo "namecol1 $GO_name_col"
+    echo "namecol2 $model_name_col"
+    python calculate_sem_sim.py --inputFile $file --nameCol1 "$GO_name_col" --nameCol2 "$model_name_col"
 
-    python calculate_semantic_similarity.py --inputFile $file --nameCol1 $GO_name_col --nameCol2 $model_name_col
-
-    python 
-    date
 done
